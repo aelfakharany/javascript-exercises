4904b511a046b00db969fae322fa85f85ee626df
Ahmad El-Fakharany <ahmadel@umich.edu>
2025-01-07 19:44:18 -0500

    Solve exercises 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+    for (let a = 0; a < valuesToRemove.length; a++) {
+        for (let b = 0; b < array.length; b++) {
+            if (valuesToRemove[a] === array[b]) {
+                array.splice(b, 1);
+                a--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let result = 0;
+    if (first < 0 
+        || second < 0 
+        || !Number.isInteger(first)
+        || !Number.isInteger(second)) 
+        {
+        return "ERROR";
+    }
+    else if (first > second) {
+        for (let i = second; i <= first; i++) {
+            result += i;
+        }
+    }
+    else {
+        for (let i = first; i <= second; i++) {
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
